cb9f33194c344873b4830579a6a4759d0f6a864b
Ryan Abernathey <rpa@ldeo.columbia.edu>
2017-03-04 20:33:59 -0500

    worked on notebooks

--- dask_jobscript.sh
+++ dask_jobscript.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+#SBATCH --account=ocp
+#SBATCH --exclusive
+#SBATCH -N 1
+#SBATCH -J dask_cluster
+#SBATCH --time=4:00:00
+
 source activate dask_distributed
-dask-scheduler --scheduler-file schedule_file & dask-worker --scheduler-file schedule_file &
+
+cd $SLURM_SUBMIT_DIR
+schedfile="$SLURM_SUBMIT_DIR/.dask_scheduler/dask_scheduler_file-$SLURM_JOBID"
+dask-scheduler --scheduler-file $schedfile &
+dask-worker --scheduler-file $schedfile &
 JUPYTER_RUNTIME_DIR=$HOME/.jupyter/runtime jupyter notebook --ip='*' --no-browser .
